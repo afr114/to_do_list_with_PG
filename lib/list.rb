7062443395f09bf7e06b8c51bf8eaa9fb5b51774
Michael Bunker & Abdul Rahimi <student@epicodus.com>
2015-08-17 13:56:49 -0700

    add the .all method to List class

--- lib/list.rb
+++ lib/list.rb
@@ -5,7 +5,18 @@ class List
     @name = attributes.fetch(:name)
     @id = attributes.fetch(:id)
   end
-  
+
+  def self.all
+   returned_lists = DB.exec("SELECT * FROM lists;")
+   lists = []
+   returned_lists.each() do |list|
+     name = list.fetch("name")
+     id = list.fetch("id").to_i()
+     lists.push(List.new({:name => name, :id => id}))
+   end
+    lists
+  end
+
 
 
 end #ends class
